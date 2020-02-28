d0a780aa4553ab7cd77f1b7b5ab6d41c1e57a1e0
Guido Günther <agx@sigxcpu.org>
2020-02-28 16:37:37 +0100

    dt-bindings: Define rfkill types
    This allows us to use them in bindings.
    
    TODO: use types in header
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>

---
+++ include/dt-bindings/rfkill/rfkill.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0-only or MIT */
+/*
+ * This header provides constants for rfkill keys bindings.
+ */
+
+#ifndef _DT_BINDINGS_RFKILL_H
+#define _DT_BINDINGS_RFKILL_H
+
+#define RFKILL_ALL 0
+#define	RFKILL_WLAN 1
+#define	RFKILL_BLUETOOTH 2
+#define	RFKILL_UWB 3
+#define	RFKILL_WIMAX 4
+#define	RFKILL_WWAN 5
+#define	RFKILL_GPS 6
+#define	RFKILL_FM 7
+#define	RFKILL_NFC 8
+#define	RFKILL_CAMERA 9
+#define	RFKILL_MIC 10
+
+#endif /* _DT_BINDINGS_RFKILL_H */
